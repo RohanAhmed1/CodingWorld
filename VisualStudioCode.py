ce3c1d4fce46a917c4c5c5c3a83b968482a21d23
Rohan Ahmed <77834806+RohanAhmed1@users.noreply.github.com>
2021-04-19 14:38:36 +0500

    Visual Studio Code guide was added

---
+++ VisualStudioCode.py
@@ -0,0 +1,31 @@
+#for creating file , we must have to specify the extension of the file. like for python file .py file should be added to the name.
+#html type has builtin suggestions
+
+#_______________________________________Tips__________________________________________________
+#Multiple Windows/Code can be at the same time.
+#ALT + (Click to desire position) to create different cursors
+#ALT + Ctrl + (Up key to add cursor in above/down) 
+#Alt + Shift + (Up/Down for adding in above/down) for copy down
+#Alt + (Up/Down for adding in above/down) moving selected line
+#select some variable and change this by occuring all recurrence
+#Snippets for custom suggestions
+#View --> Editor Layout for different screen
+#link --> https://code.visualstudio.com/docs/python/python-tutorial for understanding more features
+#Single click to provide open different file at a time first time temporarily
+#while on double click, the file will open permanently
+#_______________________________________________________________________________________________
+
+print("Farooq Ahmed Rohan Ahmed")
+print("Farooq Ahmed Rohan Ahmed")
+print("Farooq Ahmed Rohan Ahmed")
+print("Farooq Ahmed Rohan Ahmed")
+
+l=["Rohan","Rohan"]
+for i in l:
+    print(i)
+def func():
+    #jksdfjkskl
+    #klzlxkdfj
+    #sdklfsdlkfj
+    print("Hello World")
+func()    
